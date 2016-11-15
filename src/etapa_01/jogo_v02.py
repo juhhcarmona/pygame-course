5abc38d2f5ffa3cd11a463d77624040cc186aaa9
jucarmona <juhhcarmona@gmail.com>
2016-11-15 11:43:15 -0200

    Renaming folder Curso_PyGame to src and creating requirements.txt

---
+++ src/etapa_01/jogo_v02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+#
+#  Copylefth (c) 2009, Grudejo:
+#    Aline Grazielle Silva Reis
+#    Julia Carmona Almeida Chaves
+#    Luziany Maria de Oliveira
+#    Joyce Karoline Dare
+#    Prof. Douglas Machado Tavares
+#
+
+import pygame
+
+
+pygame.init()
+tela = pygame.display.set_mode((800, 600)) # tela eh uma 'surface'
+paola = pygame.image.load("paola.png") # paola eh uma 'surface'
+tela.blit(paola, (0, 100))
+print ("Ctrl+C para encerrar ;-)")
+while (True):
+    pygame.display.update()
+pygame.display.quit()
---
+++ src/etapa_01/jogo_v04.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+#
+#  Copylefth (c) 2009, Grudejo:
+#    Aline Grazielle Silva Reis
+#    Julia Carmona Almeida Chaves
+#    Luziany Maria de Oliveira
+#    Joyce Karoline Dare
+#    Prof. Douglas Machado Tavares
+#
+
+import pygame
+
+
+pygame.init()
+tela = pygame.display.set_mode((800, 600)) # tela eh uma 'surface'
+paola = pygame.image.load("paola.png") # paola eh uma 'surface'
+x_paola, y_paola = 0, 100
+print ("Ctrl+C para encerrar ;-)")
+while (True):
+    if (x_paola < 800 - 64):
+        x_paola += 4
+    tela.fill((0, 0, 0))
+    tela.blit(paola, (x_paola, y_paola))
+    pygame.display.update()
+pygame.display.quit()
